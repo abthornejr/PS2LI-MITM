71be9b3b3203d5973772c2f6832675526f029382
abthornejr <abthornejr@gmail.com>
2017-08-29 12:00:57 -0400

    Added comments. We are officially at square 0

---
+++ MainScript.R
@@ -0,0 +1,17 @@
+## MainScript.R
+##
+## The Main script... need I say more?
+
+
+
+# 1. Check PS exports inbox for most recent files
+
+# 2. If empty, alert us (e-mail) and end
+
+# 3. Call functions that manipulate the data on files that need it
+
+# 4. Upload the files to LI
+
+# 5. Rename the files with datetimestamp
+
+# 6. Move the files to the archive
